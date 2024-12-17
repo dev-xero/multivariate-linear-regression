49a5072ac780f8db66b5cf96502dda8617bec668
= <ssh.xero@gmail.com>
2024-12-17 14:16:39 +0100

    feat: Implement mulivariate linear regression

---
+++ src/np_1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([[1, 2, 4], [5, 8, 7]], dtype='float')
+print("Array created using passed list:\n", a)
+
+b = np.array((1, 3, 2))
+print("\nArray created using passed tuple:\n", b)
